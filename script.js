ab6409ca094cd5667a0e8de0bdc4d299cd06ce0e
cristian-santos <cristiansantosti@gmail.com>
2022-05-09 19:32:03 -0300

    Ajustando a dimensão do jogo

---
+++ script.js
@@ -0,0 +1,9 @@
+var altura  = 0;
+var largura = 0;
+
+function ajustaTamanhoJogo() {
+    var altura  = window.innerHeight;
+    var largura = window.innerWidth; 
+
+    console.log(altura, largura);
+}
